a8c3ea5e19a28881c92a8e2247c6d927026ffb8f
Jonatandb <jonatandb@gmail.com>
2022-06-09 19:36:30 -0300

    Added POST functionality: Creating user

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
-from psycopg2 import connect
+from flask import Flask, request, jsonify
+from psycopg2 import connect, extras
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
+key = Fernet.generate_key()
 
 host = "localhost"
 port = 5432
@@ -11,7 +13,8 @@ password = ""
 
 
 def get_connection():
-    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
+    conn = connect(host=host, port=port, dbname=dbname,
+                   user=user, password=password)
     return conn
 
 
@@ -27,7 +30,23 @@ def get_user(id):
 
 @app.post("/api/users")
 def create_user():
-    return "creating user"
+    newUser = request.get_json()
+    userName = newUser["username"]
+    email = newUser["email"]
+    password = Fernet(key).encrypt(bytes(newUser["password"], "utf-8"))
+
+    conn = get_connection()
+    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    cur.execute(
+        "INSERT INTO users (username, email, password) VALUES (%s, %s, %s) RETURNING *", (
+            userName, email, password),
+    )
+    newCreatedUser = cur.fetchone()
+
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify(newCreatedUser)
 
 
 @app.delete("/api/users/<id>")
