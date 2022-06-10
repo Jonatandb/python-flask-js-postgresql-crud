d6313702cddc2874e2664226491f5f5c69c23744
Jonatandb <jonatandb@gmail.com>
2022-06-09 21:14:38 -0300

    Added functionality to submit user data

--- static/main.js
+++ static/main.js
@@ -1,1 +1,25 @@
-console.log('Jonatandb')
+const form = document.querySelector('#userForm')
+const list = document.querySelector('#userList')
+
+form.addEventListener('submit', async e => {
+  e.preventDefault()
+  let username = form['username'].value
+  let email = form['email'].value
+  let password = form['password'].value
+
+  const response = await fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  })
+
+  const data = await response.json()
+
+  form.reset()
+})
