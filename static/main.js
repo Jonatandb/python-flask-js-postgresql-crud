4d19e9053dc6c02d630bb320488b954ca69fe9aa
Jonatandb <jonatandb@gmail.com>
2022-06-09 21:48:04 -0300

    Added functionality to show user list on page load and update list on sumbit

--- static/main.js
+++ static/main.js
@@ -1,5 +1,13 @@
 const form = document.querySelector('#userForm')
-const list = document.querySelector('#userList')
+
+let users = []
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const response = await fetch('/api/users')
+  const data = await response.json()
+  users = data
+  renderUsers(users)
+})
 
 form.addEventListener('submit', async e => {
   e.preventDefault()
@@ -20,6 +28,28 @@ form.addEventListener('submit', async e => {
   })
 
   const data = await response.json()
-
+  users.unshift(data)
+  renderUsers(users)
   form.reset()
 })
+
+function renderUsers(users) {
+  const list = document.querySelector('#userList')
+  list.innerHTML = ''
+  users.forEach(user => {
+    const userElement = document.createElement('li')
+    userElement.classList = 'list-group-item list-group-item-dark my-2'
+    userElement.innerHTML = `
+      <header class="d-flex justify-content-between align-items-center">
+        <h3>${user.username}</h3>
+        <div>
+          <button class="btn btn-danger btn-sm">Delete</button>
+          <button class="btn btn-secondary btn-sm">Edit</button>
+        </div>
+      </header>
+      <p>${user.email}</p>
+      <p class="text-truncate">${user.password}</p>
+    `
+    list.append(userElement)
+  })
+}
